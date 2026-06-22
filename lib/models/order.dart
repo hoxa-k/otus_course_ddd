45f1b18bf4e6118ef5f8e220372fc7cf39bd54c2
Куликова Наталья Павловна <kulikova_n@tass.ru>
2026-06-22 11:21:10 +0300

    Order aggregate

---
+++ lib/models/order.dart
@@ -0,0 +1,39 @@
+import 'order_line.dart';
+import 'product.dart';
+
+class Order {
+  static const maxPositionsCount = 10;
+  final String id;
+  final List<OrderLine> _orderLines = [];
+
+  Order({required this.id});
+
+  List<OrderLine> get items => _orderLines;
+
+  void addItem(Product product, int quantity) {
+    final bool isContainProduct = _isContainProduct(product);
+    if (_orderLines.length >= 10 && !isContainProduct) {
+      throw Exception(
+        'В заказ нельзя добавить больше $maxPositionsCount позиций',
+      );
+    }
+
+    final orderLine = OrderLine(productId: product.id, quantity: quantity);
+    if (isContainProduct) {
+      final productIndex = _orderLines.indexWhere(
+        (e) => e.productId == product.id,
+      );
+      if (productIndex >= 0) _orderLines.removeAt(productIndex);
+      _orderLines.insert(productIndex, orderLine);
+    } else {
+      _orderLines.add(orderLine);
+    }
+  }
+
+  bool _isContainProduct(Product product) {
+    for (final orderLine in _orderLines) {
+      if (orderLine.productId == product.id) return true;
+    }
+    return false;
+  }
+}
---
+++ lib/models/order_line.dart
@@ -0,0 +1,9 @@
+class OrderLine {
+  final String productId;
+  final int quantity;
+
+  const OrderLine({
+    required this.productId,
+    required this.quantity,
+  });
+}
