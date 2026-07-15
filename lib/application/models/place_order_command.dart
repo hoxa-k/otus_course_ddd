fb18830637a8d8eed38b26e77f3b987e490032e6
Куликова Наталья Павловна <kulikova_n@tass.ru>
2026-07-15 11:42:32 +0300

    remove unused PlaceOrderCommand

--- lib/application/models/place_order_command.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ddd/application/interfaces/i_command.dart';
-
-class PlaceOrderCommand implements ICommand {
-  final String id;
-  final String orderId;
-
-  const PlaceOrderCommand({required this.id, required this.orderId});
-}
